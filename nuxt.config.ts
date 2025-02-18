9fac038d91e2ab116e976915073be6efed0ab36c
Jamiemufu <taichi.chikung@gmail.com>
2025-02-18 01:58:35 +0000

    Added openAI docs and scalar. Reset/seed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@scalar/nuxt'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  typescript: {
-    typeCheck: true
-  }
+  nitro: {
+    experimental: {
+      openAPI: true
+    }
+  },
 })
