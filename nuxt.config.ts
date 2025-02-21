48896569e6fd8a6fa69da7f6714be9f9a4e0b604
Jamiemufu <taichi.chikung@gmail.com>
2025-02-21 14:45:47 +0000

    remove scalar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@scalar/nuxt', '@prisma/nuxt'],
+  modules: ['@prisma/nuxt'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   nitro: {
